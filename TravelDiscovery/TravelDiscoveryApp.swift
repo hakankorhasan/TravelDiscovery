1dc16e2a8f30fe5d4f18edfdf1d348185233a78f
Hakan Körhasan <hakankorhasann@gmail.com>
2022-11-28 20:00:52 +0300

    Initial Commit

---
+++ TravelDiscovery/TravelDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryApp.swift
+//  TravelDiscovery
+//
+//  Created by Hakan Körhasan on 28.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
